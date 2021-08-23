be4eba4f5b7185d80bd6285ae5793d35435a544e
clarence@cannon.cloud <clarence@cannon.cloud>
2021-08-23 14:10:48 -0700

    dashboard added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 
 import Entry from "./pages/entry/Entry.page";
 import DefaultLayout from "./layout/DefaultLayout";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div>
-      <DefaultLayout>DASHBOARD</DefaultLayout>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   );
 }
